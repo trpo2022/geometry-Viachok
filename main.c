0f1a2d515580f2d97b341e48846e1ddb24513f65
Шрейдер Вячеслав Романович <iv122s27@bwp16.csc.local>
2022-03-15 14:12:36 +0700

    main.c is now formatted by .clang-format

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-int GetFormat(char string[],int *arr)
+int GetFormat(char string[], int* arr)
 {
     int cords[3];
-    int *adres = arr;
-    if(!strcmp(string,"circle")){
-        if(scanf("%d %d %d",&cords[0],&cords[1],&cords[2]) == 3)
-        {
+    int* adres = arr;
+    if (!strcmp(string, "circle")) {
+        if (scanf("%d %d %d", &cords[0], &cords[1], &cords[2]) == 3) {
             *adres = cords[0];
             adres++;
             *adres = cords[1];
@@ -24,16 +23,17 @@ int main()
     char format[30];
     int cords[3];
     printf("Type !exit to stop the programm.\n");
-    while(1){
-            scanf("%s",format);
-        if(!strcmp(format,"!exit"))
+    while (1) {
+        scanf("%s", format);
+        if (!strcmp(format, "!exit"))
             break;
-        if(GetFormat(format,cords))
-        {
-            printf("Object: %s\nX: %d\nY: %d\nRadius: %d\n",format,cords[0],cords[1],cords[2]);
-        }
-        else
-        {
+        if (GetFormat(format, cords)) {
+            printf("Object: %s\nX: %d\nY: %d\nRadius: %d\n",
+                   format,
+                   cords[0],
+                   cords[1],
+                   cords[2]);
+        } else {
             printf("Incorrect Format\n");
         }
     }
